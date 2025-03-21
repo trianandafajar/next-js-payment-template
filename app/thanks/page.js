8ab92ad2ebf305dba254f96119ac1421ead6216c
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-21 09:34:58 +0700

    Update Format Succes

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,13 +1,15 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
-    return (
-        <div className='flex flex-col justify-center items-center min-h-screen gap-4'>
-            <h3 className='text-center'>Terimakasih telah melakukan pembayaran</h3>
-            <Link href="/" className="text-center">Kembali</Link>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+      <h3 className="text-center">Terimakasih telah melakukan pembayaran</h3>
+      <Link href="/" className="text-center">
+        Kembali
+      </Link>
+    </div>
+  );
+};
 
-export default page
+export default page;
