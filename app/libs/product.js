ec9d1d9ea48a9b392dbfeb82122c33442a50e83a
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-21 09:34:33 +0700

    Udpate Product

--- app/libs/product.js
+++ app/libs/product.js
@@ -1,13 +1,14 @@
 export const product = {
-    id: ~~(Math.random() * 100) + 1,
-    image: "/img.png",
-    name: "LEVI'S® WOMEN'S XL TRUCKER JACKET",
-    price: 350000,
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum",
-    colors: [
-        { value: "putih", label: "Putih" },
-        { value: "biru", label: "Biru" },
-        { value: "coklat", label: "Coklat" },
-        { value: "kuning", label: "Kuning" }
-    ]
-}
+  id: ~~(Math.random() * 100) + 1,
+  image: "/img.png",
+  name: "LEVI'S® WOMEN'S L TRUCKER JACKET",
+  price: 300000,
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum",
+  colors: [
+    { value: "putih", label: "Putih" },
+    { value: "biru", label: "Biru" },
+    { value: "coklat", label: "Coklat" },
+    { value: "merah", label: "Merah" },
+  ],
+};
