a369c3e185226f12a2695ebbd824844c0391bc2c
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-25 22:59:31 +0700

    Update Product

--- app/libs/product.js
+++ app/libs/product.js
@@ -1,10 +1,10 @@
 export const product = {
-  id: ~~(Math.random() * 100) + 1,
+  id: Math.floor(Math.random() * 100) + 1,
   image: "/img.png",
   name: "LEVI'S® WOMEN'S L TRUCKER JACKET",
-  price: 300000,
+  price: 300_000,
   description:
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum.",
   colors: [
     { value: "putih", label: "Putih" },
     { value: "biru", label: "Biru" },
