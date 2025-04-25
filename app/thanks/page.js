e2719e7beba9042258978768e79c3a2aef2f2ca1
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-25 22:59:19 +0700

    Update Thanks Page

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,15 +1,12 @@
 import Link from "next/link";
-import React from "react";
 
-const page = () => {
+export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
-      <h3 className="text-center">Terimakasih telah melakukan pembayaran</h3>
-      <Link href="/" className="text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h3 className="text-center">Terima kasih telah melakukan pembayaran</h3>
+      <Link href="/" className="text-blue-600 hover:underline">
         Kembali
       </Link>
     </div>
   );
-};
-
-export default page;
+}
